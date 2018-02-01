ca6e045cf6c81eb19e9b22b2a2d4e3fc36e96be5
Adam Reynolds <adam.reynolds@grapeshot.com>
2018-02-01 17:37:30 +0000

    Completed CoreData 6

---
+++ Get to it!/CreateTaskViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreateTaskViewController.swift
+//  Get to it!
+//
+//  Created by Adam Reynolds on 01/02/2018.
+//  Copyright © 2018 Adam Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
---
+++ Get to it!/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Get to it!
+//
+//  Created by Adam Reynolds on 01/02/2018.
+//  Copyright © 2018 Adam Reynolds. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
